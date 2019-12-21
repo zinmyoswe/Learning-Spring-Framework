a51ecf9bd17ad6286d890d40dff7eb8e26f1fa97
ZIN MYO <overmidnight12@gmail.com>
2019-12-20 23:33:17 -0800

    first commit

---
+++ src/main/java/com/thura/zayarmyat/Bike.java
@@ -0,0 +1,7 @@
+package com.thura.zayarmyat;
+
+public class Bike implements Vehicle {
+	public void drive() {
+		System.out.println("Riding Bike");
+	}
+}
---
+++ src/main/java/com/thura/zayarmyat/Car.java
@@ -0,0 +1,7 @@
+package com.thura.zayarmyat;
+
+public class Car implements Vehicle{
+	public void drive() {
+		System.out.println("Fast and Furious!!");
+	}
+}
